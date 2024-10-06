aa682127d7fbc4db1eaf0188821ea18e156ed1b3
Rohan Arora <rohanaroraaashi@gmail.com>
2024-10-06 22:08:31 +0530

    Update get-users.py

--- get-users.py
+++ get-users.py
@@ -121,9 +121,9 @@ def extract_users(driver, link):
 
 
 def main():
-    user_name = input("lifefacctss")
-    password = input("lifefacctss@#@#1999")
-    url = input("https://www.instagram.com/p/C3m8qzGPJLp/")
+    user_name ="lifefacctss"
+    password ="lifefacctss@#@#1999"
+    url ="https://www.instagram.com/p/C3m8qzGPJLp/"
 
     driver = webdriver.Edge(executable_path='msedgedriver.exe')
 
